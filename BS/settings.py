54d0f17abd223fc639804c763965a9d25c198ed1
Aanandi Pankhania <88778753+Aanandi2802@users.noreply.github.com>
2022-03-20 22:57:39 +0530

    admin site change

--- BS/settings.py
+++ BS/settings.py
@@ -157,3 +157,29 @@ EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Default from Email'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+JAZZMIN_SETTINGS = {
+    # title of the window (Will default to current_admin_site.site_title if absent or None)
+    "site_title": "AdMe Admin",
+
+    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_header": "AdMe Admin",
+
+    # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_brand": "AdMe"
+   
+    # Logo to use for your site, must be present in static files, used for brand on top left
+    #"site_logo": "books/img/logo.png",
+
+    # CSS classes that are applied to the logo above
+    #"site_logo_classes": "img-circle",
+
+    # Welcome text on the login screen
+    #"welcome_sign": "Welcome to the AdMe Admin-Page...",
+
+    # Copyright on the footer
+    #"copyright": "Adme- iViewLabs Pvt Ltd"
+
+    
+}
+
