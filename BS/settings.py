96191b805bcf55c98d1ecc6e7d6831e074983b81
Aanandi Pankhania <88778753+Aanandi2802@users.noreply.github.com>
2022-03-21 10:49:16 +0530

    change in templates

--- BS/settings.py
+++ BS/settings.py
@@ -169,7 +169,7 @@ JAZZMIN_SETTINGS = {
     "site_brand": "AdMe",
    
     # Logo to use for your site, must be present in static files, used for brand on top left
-    #"site_logo": "books/img/logo.png",
+    #site_logo": "BS\images\logo.png",
 
     # CSS classes that are applied to the logo above
     #"site_logo_classes": "img-circle",
